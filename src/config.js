c2d82cfb31b730c13281060dd3e698a17daf6ee9
Yajaira Flynn-Corbin <yary.ba26@gmail.com>
2020-04-22 14:54:30 -0400

    fixes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,11 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  API_TOKEN: process.env.API_TOKEN || "dummy-api-token",
-  DB_URL:
-    process.env.DB_URL || "postgres://dunder_mifflin@localhost:5432/bookmarks",
+  //API_TOKEN: process.env.API_TOKEN || "dummy-api-token",
+  // //DATABASE_URL:
+  //     process.env.DATABASE_URL ||
+  //     "postgres://dunder_mifflin@localhost:5432/bookmarks",
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/bookmarks",
 };
